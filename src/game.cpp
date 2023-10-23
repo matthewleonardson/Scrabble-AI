8df881255e7e028710dcb1ba319d38a0443b2f08
matthewleonardson <matthewcleonardson@gmail.com>
2023-10-23 10:44:49 -0500

    Created player struct

--- src/game.cpp
+++ src/game.cpp
@@ -1,9 +1,28 @@
 #include "game.hpp"
 
+Game::Player::Player() {
+
+    score = 0;
+    tiles.resize(7);
+
+}
+
+Game::Player::Player(std::vector<char> starting_tiles) {
+
+    score = 0;
+    
+    for(char tile : starting_tiles) {
+        tiles.push_back(tile);
+    }
+
+}
+
 Game::Game(std::string dict_file_path) {
 
     dict_ = new Dictionary(dict_file_path);
     board_ = new Board();
-    scores_ = {0, 0};
+    players_.first = Player();
+    players_.second = Player();
 
 }
+
--- src/game.hpp
+++ src/game.hpp
@@ -5,12 +5,21 @@
 class Game {
 
 public:
-    Game() = default;
+
+    Game() = default; 
     Game(std::string dict_file_path);
 
 private:
+
+    struct Player {
+        Player();
+        Player(std::vector<char> starting_tiles);
+        unsigned score = 0;
+        std::vector<char> tiles;
+    };
+
     Dictionary* dict_;
     Board* board_;
-    std::pair<unsigned, unsigned> scores_;
+    std::pair<Player, Player> players_;
 
 };
